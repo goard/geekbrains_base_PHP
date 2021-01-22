3e5617ee26b2177e69caff00d675db40ac5d3337
Gonchikov Ayur <hori.tumat@gmail.com>
2021-01-22 13:23:39 +0800

    remove file

--- index1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$a = 0;
-$b = 0;
-
-if ($a>=0 && $b>=0) return var_dump($a-$b);
-if ($a<0 && $b<0) return var_dump($a*$b);
-if (($a<0 && $b>=0) ||($a>=0 && $b<0)) return var_dump($a+$b);
--- index6.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function power(float $val = 0, int $pow = 1) : float {
-  return $val *= $pow == 1 ? 1 :power($val, $pow-1);
-}
-
-var_dump(power(3,3));
