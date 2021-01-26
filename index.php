69731a6a0a5dd5ff228fb50d673d9f157588e1fe
Gonchikov Ayur <hori.tumat@gmail.com>
2021-01-26 17:06:43 +0800

    hw for lesson4

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+if (!empty($_FILES['img'])) {
+  // var_dump($_FILES["img"]);
+  $upload_dir = 'img/';
+  $tmpFile = $_FILES["img"];
+  if ($tmpFile["size"] <= 2097152)
+    move_uploaded_file($tmpFile["tmp_name"], $upload_dir . $tmpFile["name"]);
+  else echo "Превышен предел загрузки файла в 2 МБ";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <main>
+    <div>
+      <form enctype="multipart/form-data" method="POST">
+        <input type="file" id="img" name="img" accept="image/png,image/jpeg" />
+        <input type="submit" />
+      </form>
+      <div>
+        <?php
+        $arrImg = scandir("img/");
+        for ($i = 2; $i < count($arrImg); $i++) {
+          echo "<a href=\"img/$arrImg[$i]\" target=\"_blank\"> <img src=\"img/$arrImg[$i]\" alt= \"image\" width=\"450px\" /> </a> <br/>";
+        };
+        ?>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
--- index5.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function transformStr(string $str) : string {
-  return str_replace(" ", "_", $str);
-}
-
-var_dump(transformStr("Привет лунатикам привет мир!"));
